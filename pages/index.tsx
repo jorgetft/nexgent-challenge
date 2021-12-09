be29fc4f25c7b5133d17d58e90ef35f29507cd05
Jorge Rivera <jorge@Mackbook-de-Jorge.local>
2021-12-09 12:27:20 -0500

    First task completed (students page)

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,32 +8,20 @@ type Props = {};
 
 const Main: React.FC<Props> = ({}) => {
   const [students, setStudents] = useState([]);
-    const [allStudents, setAllStudents] = useState([]);
-    const [search, setSearch] = useState('');
 
     useEffect(() => {
       fetchStudents().then((resp: Student[]) => {
        setStudents(resp)
-        //Get back all students when search query not match any user
-       setAllStudents(resp)
       })
     }, []);
 
     const onChangeHandler = (event: any) => {
-      const {value} = event.target
- 
-      setSearch(value)
-      const filtered = students.filter((s:Student) => s.first_name.toLocaleLowerCase() === value.toLocaleLowerCase() || s.last_name.toLocaleLowerCase() === value.toLocaleLowerCase())
-      if(filtered.length > 0) {
-        setStudents(filtered)
-      } else {
-        setStudents(allStudents)
-      }
-   };
+      // TODO
+    };
 
    return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
-      <TextInput placeholder="type here" value={search} onChange={onChangeHandler} />
+      <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
       <Box direction="row" wrap={true}>
         {students.map((s:Student) => (
           <Link key={s.id}  href={`/student/${s.id}`}>
