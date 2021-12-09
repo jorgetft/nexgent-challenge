46aa446b9356e8b3d4d3925e5a7dfc552bf93e9f
Jorge Rivera <jorge@Mackbook-de-Jorge.local>
2021-12-09 12:22:40 -0500

    First task completed (students page)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,47 @@
 import { Box, TextInput } from "grommet";
 import React, { useEffect, useState } from "react";
+import Link from 'next/link'
 import UserCard from "../components/user.card";
 import { fetchStudents, Student } from "../services/students";
 
 type Props = {};
 
 const Main: React.FC<Props> = ({}) => {
-  const onChangeHandler = (event: any) => {
-    // TODO
-  };
+  const [students, setStudents] = useState([]);
+    const [allStudents, setAllStudents] = useState([]);
+    const [search, setSearch] = useState('');
 
-  return (
+    useEffect(() => {
+      fetchStudents().then((resp: Student[]) => {
+       setStudents(resp)
+        //Get back all students when search query not match any user
+       setAllStudents(resp)
+      })
+    }, []);
+
+    const onChangeHandler = (event: any) => {
+      const {value} = event.target
+ 
+      setSearch(value)
+      const filtered = students.filter((s:Student) => s.first_name.toLocaleLowerCase() === value.toLocaleLowerCase() || s.last_name.toLocaleLowerCase() === value.toLocaleLowerCase())
+      if(filtered.length > 0) {
+        setStudents(filtered)
+      } else {
+        setStudents(allStudents)
+      }
+   };
+
+   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
-      <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
+      <TextInput placeholder="type here" value={search} onChange={onChangeHandler} />
       <Box direction="row" wrap={true}>
-        {/* {students.map((s) => (
+        {students.map((s:Student) => (
+          <Link key={s.id}  href={`/student/${s.id}`}>
           <Box margin="10px">
-            <UserCard user={s} />
+              <UserCard user={s} />
           </Box>
-        ))} */}
+          </Link>
+        ))}
       </Box>
     </Box>
   );
